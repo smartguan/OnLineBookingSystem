c438a480019bc06c9c86752b43dfb8007f86ecd0
ulises <ulises.lerm@gmail.com>
2013-03-12 11:49:06 -0700

    Homepage-Ver1.0

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,9 @@
 Cs169::Application.routes.draw do
+  
+  root :to => "homepage#homepage"
+
+  get "homepage/homepage"
+
   get "users/add"
 
   get "users/login"
@@ -16,7 +21,7 @@ Cs169::Application.routes.draw do
   match '/Users/delete', to: 'users#delete'
   match '/Users/update', to: 'users#update'
   match '/Users/profile', to: 'users#profile'
-  
+    
   # Routes for Registration controller
   match '/registrations/schedule', to: 'registrations#getSchedule', :via => :get, :default => {format: "json"}
   match '/admin/createSection', to:'registrations#createSection', :via => :post, :default => {format: "json"}
