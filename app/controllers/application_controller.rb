7dae76db322565461012dedb3581c84d21acbb0c
Seth <seth@ubuntu.(none)>
2013-03-19 11:26:47 -0700

    1. Rspec bug in User_controller fixed

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,16 +1,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
-  protected
+  #protected
 
-    def verify_authenticity_token
-      unless verified_request? || json_request?
-        logger.warn "Can't verify CSRF token authenticity" if logger
-        handle_unverified_request
-      end
-    end
+  #  def verify_authenticity_token
+  #    unless verified_request? || json_request?
+  #      logger.warn "Can't verify CSRF token authenticity" if logger
+  #      handle_unverified_request
+  #    end
+  #  end
 
-    def json_request?
-      request.format.json?
-    end
+  #  def json_request?
+  #    request.format.json?
+  #  end
 end
