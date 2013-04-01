b7bf790e0040caadc2ad6014dc1b81a6e1099a69
Daniel <daniel@toshi808.(none)>
2013-04-01 00:07:08 -0700

    added some css to the users add section mostly to add hover feature so user knows information toggles on click

---
+++ spec/views/users_update_password_spec.rb
@@ -0,0 +1,55 @@
+require 'spec_helper'
+
+describe UsersUpdatePassword do
+	SUCCESS = 1
+	FIRST_NOT_VALID = 101
+	LAST_NOT_VALID = 102
+	EMAIL_NOT_VALID = 103
+	PASS_NOT_VALID = 104
+	PASS_NOT_MATCH = 109
+	DOB_NOT_VALID = 105
+	ZIP_NOT_VALID = 106
+	USER_EXISTS = 107
+	BAD_CREDENTIALS = 108
+
+	describe "updating user's password", :js => true do
+		before(:each) do
+			visit "users/update_password"
+		end
+
+		describe "leaving empty fields" do
+			before(:each) do 
+		    	fill_in "old-password", with: "oldpassword"
+		    	fill_in "password", with: "newpassword"
+		    	fill_in "password-confirm", with: "newpassword"
+		    end
+
+		  	it "should render empty old password error" do
+		  		fill_in "old-password", with: ""
+		  		click_button "test-update"
+		    	page.should have_content "invalid"
+		  	end
+
+		  	it "should render empty new password error" do
+		  		fill_in "password", with: ""
+		  		click_button "test-update"
+		    	page.should have_content "invalid"
+		  	end
+
+		  	it "should render empty new password error" do
+		  		fill_in "password-confirm", with: ""
+		  		click_button "test-update"
+		    	page.should have_content "invalid"
+		  	end
+		end
+
+		describe "passwords do not match" do
+
+		end
+
+
+	end
+
+
+
+end
