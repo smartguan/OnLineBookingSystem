447a655e453f2a34c930ab1c31e89749c28d2f94
Huy Le <le.davidm@gmail.com>
2013-04-18 23:01:48 -0700

    fucked around with the admin page, it hella sucks

--- app/assets/javascripts/admin.js
+++ app/assets/javascripts/admin.js
@@ -41,7 +41,6 @@ function get_json_request(page, dict, success) {
 
 function handle_add_response(data) {
   if (data.errCode == 1) {
-    $('#new-form').hide();
     $('#edit-form').hide();
     $('#section-table').fadeOut('slow', function() {
         post_json_request("/Sections/getAllSections", {}, function(data) {loadingSections(data)});
@@ -56,10 +55,10 @@ function handle_add_response(data) {
 
 function handle_edit_response(data) {
     if (data.errCode == 1) {
-        alert(data.sections[0].teacher);
-    $('#edit-form').loadJSON(data.sections[0]);
+        //alert(data.sections[0].teacher);
+    $('#new-form3').loadJSON(data.sections[0]);
     //post_json_request("/Registrations/getSchedule", {}, function(data) {loadingBody(data)});
-    $('#edit-form').toggle();
+     $('#new-form3').toggle();
   } else {
     alert(translateErrCode(data.errCode));
   }
